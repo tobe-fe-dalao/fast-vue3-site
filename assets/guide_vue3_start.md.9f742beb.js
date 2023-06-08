974b8ea02beed34bc61f3329916f9bc293d95ed2
fonghehe <fonghehe@users.noreply.github.com>
2023-06-08 03:26:44 +0000

    deploy: 457746a317a631dfbc6a654d33b12032ca14d7de

--- assets/guide_vue3_start.md.9f742beb.js
+++
@@ -1,8 +0,0 @@
-import{_ as s,c as a,o as n,O as e}from"./chunks/framework.93b9f0ce.js";const u=JSON.parse('{"title":"启动项目","description":"","frontmatter":{},"headers":[],"relativePath":"guide/vue3/start.md","filePath":"guide/vue3/start.md","lastUpdated":1682422738000}'),t={name:"guide/vue3/start.md"},l=e(`<h1 id="启动项目" tabindex="-1">启动项目 <a class="header-anchor" href="#启动项目" aria-label="Permalink to &quot;启动项目&quot;">​</a></h1><div class="language-"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki material-theme-palenight"><code><span class="line"><span style="color:#A6ACCD;">// 拉取项目</span></span>
-<span class="line"><span style="color:#A6ACCD;">git clone https://github.com/tobe-fe-dalao/fast-vue3</span></span>
-<span class="line"><span style="color:#A6ACCD;"></span></span>
-<span class="line"><span style="color:#A6ACCD;">// 安装依赖</span></span>
-<span class="line"><span style="color:#A6ACCD;">yarn install  or  pnpm install(不推荐使用npm)</span></span>
-<span class="line"><span style="color:#A6ACCD;"></span></span>
-<span class="line"><span style="color:#A6ACCD;">// 启动项目</span></span>
-<span class="line"><span style="color:#A6ACCD;">yarn dev  or  pnpm dev</span></span></code></pre></div>`,2),p=[l];function o(c,r,i,d,_,C){return n(),a("div",null,p)}const h=s(t,[["render",o]]);export{u as __pageData,h as default};
